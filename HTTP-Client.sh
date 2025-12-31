c2557bc0ed87968f351918b096a8f040fd759be0
Maoyin Sun <simonmysun@gmail.com>
2025-12-31 02:24:05 +0100

    fix: replace `hostname` with POSIX compliant `uname -a`

--- HTTP-Client.sh
+++ HTTP-Client.sh
@@ -18,7 +18,7 @@ fi
 
 # Functions
 GetEnv() {
-   usr="$(whoami | tr "[:upper:]" "[:lower:]")@$(hostname | tr "[:upper:]" "[:lower:]")"
+   usr="$(whoami | tr "[:upper:]" "[:lower:]")@$(uname -n | tr "[:upper:]" "[:lower:]")"
    pwd="$pwdnew"
    echo "$usr!$pwd"
 }
